a5961f9e7abe3644d6752cb5daf2cfd01b5588f0
faronan <toshiki.ito88@gmail.com>
2020-03-16 19:32:00 +0900

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 echo ".devcontainer" >> .gitignore
 echo ".vscode" >> .gitignore
-echo "setup.sh" >> .gitignore
 
 export $(cat .env)
 rm .env
+rm setup.sh
 
-grep -l 'base' Cargo.toml .vscode/launch.json  | xargs sed -i "s/base/${PACKAGE_NAME}/g"
+grep -l 'base' Cargo.toml .vscode/launch.json  | xargs sed -i "" "s/base/${PACKAGE_NAME}/g"
+
+cargo update
 
 git add .
-git commit
+git commit -m "initial commit"
